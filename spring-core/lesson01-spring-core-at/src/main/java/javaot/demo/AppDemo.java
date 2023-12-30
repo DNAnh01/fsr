5ad6d716158260e948e80475b2f9a13e09f32360
DNAnh01 <donguyenanhgithub@gmail.com>
2023-12-31 03:09:11 +0700

    update folder structure

--- spring-core/lesson01-spring-core-at/src/main/java/javaot/demo/AppDemo.java
+++ spring-core/lesson01-spring-core-at/src/main/java/javaot/demo/AppDemo.java
@@ -12,6 +12,9 @@ public class AppDemo {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(LOCATION);
+		System.out.println("\u001B[33m" + "App is working!!" + "\u001B[0m");
+		System.out.println("\u001B[32m" + "App is working!!" + "\u001B[0m");
+		System.out.println("\u001B[34m" + "App is working!!" + "\u001B[0m");
 //		ContextUtils.showBeansName(context);	
 		MovieRecommender mr = context.getBean("movieRecommender", MovieRecommender.class);
 		mr.recommend();
--- spring-core/lesson01-spring-core-at/src/main/java/javaot/utils/ContextUtils.java
+++ spring-core/lesson01-spring-core-at/src/main/java/javaot/utils/ContextUtils.java
@@ -6,16 +6,17 @@ import org.springframework.context.ApplicationContext;
 
 public class ContextUtils {
 	private ContextUtils() {
-		
+
 	}
-	
+
 	public static String[] getBeansName(ApplicationContext context) {
 		return context.getBeanDefinitionNames();
 	}
-	
+
 	// Show id of beans in current context(container)
 	public static void showBeansName(ApplicationContext context) {
-//		System.out.println(Arrays.toString(getBeansName(context)));
+		// System.out.println(Arrays.toString(getBeansName(context)));
 		Arrays.stream(getBeansName(context)).forEach(System.out::println);
+
 	}
 }
